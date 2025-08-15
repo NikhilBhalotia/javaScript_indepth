9e60b53d9c01367f1e011fa3bd7ae4e31dfe5357
Nikhil Bhalotia <nikhilbhalotia21@gmail.com>
2025-08-15 03:21:03 +0000

    preoject 2 changes

---
+++ 07_projects/project02/script.js
@@ -0,0 +1,19 @@
+const form = document.querySelector('form');
+    
+    form.addEventListener(('submit'),(e)=>{
+      e.preventDefault();
+      const height = parseInt(document.querySelector('#height').value);
+      const weight = parseInt(document.querySelector('#weight').value);
+      console.log(typeof height);
+      console.log(weight);
+      if(height ===''||height<0||isNaN(height) || weight ==='' || weight<0 || isNaN(weight)){
+        const results = document.querySelector('#results');
+        results.innerHTML = "Please Enter Valid Input";
+        
+      }
+      else{
+        
+        const results = document.querySelector('#results');
+        results.innerHTML = ((weight/(height*height))*10000).toFixed(2);
+      }
+    })
