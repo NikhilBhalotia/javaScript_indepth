c03f985e7c43182a0281a2ca5b955b8c7066c7c1
Nikhil Bhalotia <nikhilbhalotia21@gmail.com>
2025-08-16 06:01:11 +0000

    update solution md

--- 07_projects/project04/script.js
+++ 07_projects/project04/script.js
@@ -1,68 +1,3 @@
-## project 1
-
-```javascript
-
-let btn = document.querySelectorAll(".button");
-      btn.forEach((val) => {
-        val.addEventListener("click", (e) => {
-          const attri = val.getAttribute("id");
-          document.body.style.backgroundColor = attri;
-        });
-      });
-
-```
-
-## project 2
-
-```javascript
-
-const form = document.querySelector('form');
-    
-    form.addEventListener(('submit'),(e)=>{
-      e.preventDefault();
-      const height = parseInt(document.querySelector('#height').value);
-      const weight = parseInt(document.querySelector('#weight').value);
-      console.log(typeof height);
-      console.log(weight);
-      if(height ===''||height<0||isNaN(height) || weight ==='' || weight<0 || isNaN(weight)){
-        const results = document.querySelector('#results');
-        results.innerHTML = "Please Enter Valid Input";
-        
-      }
-      else{
-        
-        const results = document.querySelector('#results');
-        results.innerHTML = ((weight/(height*height))*10000).toFixed(2);
-      }
-    })
-
-
-```
-
-## project 3
-
-```javascript
-
-const guessNum = document.querySelector("#guessField");
-const randomNum = Math.floor((Math.random()*100)+1)
-console.log(randomNum);
-
-const submit = document.querySelector("#subt");
-
-submit.addEventListener('submit',(e)=>{
-    let numberGuessed = guessNum.innerHTML
-    console.log(numberGuessed);
-    
-})
-
-
-
-```
-
-
-## project 4
-
-```javascript
 let guessingField = document.querySelector("#guessField");
 let submitguess = document.querySelector("#subt");
 let remainGuesses = document.querySelector(".lastResult");
@@ -145,6 +80,3 @@ function updateHtml(guessNumber, text) {
   remainGuesses.appendChild(output);
   output.innerHTML = `${text}`;
 }
-
-
-```
